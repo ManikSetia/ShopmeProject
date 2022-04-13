767e3ecd4035dc39c6f264d3aa68deda1a394ce6
Manik <manik.setia001@gmail.com>
2022-04-13 02:40:03 +0200

    Added functionality of edit button

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,6 +36,7 @@ public class UserController {
         //This is passed to map the values of form attributes to our instance variables of User class.
         model.addAttribute("user", user);
         model.addAttribute("roles", roles);
+        model.addAttribute("pageTitle", "Create New User");
         return "user_form";
     }
 
@@ -42,12 +44,32 @@ public class UserController {
     public String saveUser(User user, RedirectAttributes redirectAttributes){
         //User object is passed because we want to map the values coming from form to the instance variables of User class
         //RedirectAttributes is used to display the successful message, i.e. we're sending this attribute while redirecting the page.
+        //We can send message while redirecting.
 //        System.out.println(user);
 
         userService.save(user);
 
         redirectAttributes.addFlashAttribute("saveMessage", "The user has been added successfully.");
         return "redirect:/users";//if redirect is not used, then we'll have to refresh the page after saving the object
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String editUser(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes){
+        try{
+            User user=userService.get(id);
+            List<Role> roles=userService.listRoles();
+            model.addAttribute(roles);
+            model.addAttribute(user);
+            model.addAttribute("roles", roles);
+            model.addAttribute("pageTitle", "Edit User (ID: "+id+")");
+//            model.addAttribute("enabled", user.isEnabled());
+            return "user_form";
+        }
+        catch (UserNotFoundException ex){
+            redirectAttributes.addFlashAttribute("userNotFoundMessage", ex.getMessage());
+            return "redirect:/users";
+        }
+
     }
 
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -30,7 +31,26 @@ public class UserService {
     }
 
     public void save(User user){
-        encodePassword(user);
+        boolean isUpdatingUser= (user.getId() != null);
+
+        if(isUpdatingUser){
+
+            //There can be 2 cases:
+                //One is user wants to change the password
+                //Second is user doesn't wants to change the password
+            User existingUser=userRepository.findById(user.getId()).get();
+            if(user.getPassword().isEmpty()){
+                user.setPassword(existingUser.getPassword());
+            }
+            else{
+                encodePassword(user);
+            }
+        }
+        else {
+            //new user
+            encodePassword(user);
+        }
+
         userRepository.save(user);
     }
 
@@ -38,8 +58,29 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
     }
 
-    public boolean isEmailUnique(String email){
-        User user=userRepository.getUserByEmail(email);
-        return user==null;
+    public boolean isEmailUnique(Integer id, String email){
+        User userByEmail=userRepository.getUserByEmail(email);
+
+        if(userByEmail == null) return true;
+
+        boolean isCreatingNew= (id==null);
+
+        if(isCreatingNew){
+            if(userByEmail != null) return false;
+        }
+        else{
+            if(userByEmail.getId() != id) return false;
+        }
+
+        return true;
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        try{
+            return userRepository.findById(id).get();
+        }
+        catch(NoSuchElementException ex){
+            throw new UserNotFoundException("Could not find any user with ID: "+id);
+        }
     }
 }
